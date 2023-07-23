bd2f587f4e03006f62950a34cdb0f2a5c37b8e1e
kenser <koffy140221@gmail.com>
2023-07-22 21:37:04 -0600

    crear las tablas en base de datos

--- base_de_Datos.sql
+++ base_de_Datos.sql
@@ -5,3 +5,9 @@ create table productos(
     producto_situacion smallint not null default 1,
     primary key (producto_id)
 )
+Create table clientes (
+    cliente_id serial primary key,
+    cliente_nombre varchar (60) not null,
+    cliente_nit varchar (10) not null,
+    cliente_situacion char (1) DEFAULT '1'
+);
