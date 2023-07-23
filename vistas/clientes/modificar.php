0481c936f64fc92b90ee4bcadec5b97ba6026de9
kenser <koffy140221@gmail.com>
2023-07-22 23:49:42 -0600

    crear odificar

--- vistas/clientes/modificar.php
+++ vistas/clientes/modificar.php
@@ -19,7 +19,7 @@ require '../../modelos/Cliente.php';
     <div class="container">
         <h1 class="text-center">Modificar Clientes</h1>
         <div class="row justify-content-center">
-            <form action="/caaljuc_tarea6/controladores/productos/modificar.php" method="POST" class="col-lg-8 border bg-light p-3">
+            <form action="/caaljuc_tarea6/controladores/clientes/modificar.php" method="POST" class="col-lg-8 border bg-light p-3">
                 <input type="hidden" name="cliente_id" value="<?= $clientes[0]['CLIENTE_ID'] ?>" >
                 <div class="row mb-3">
                     <div class="col">
